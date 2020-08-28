a9fc919c81a68e78353486293959fd9c798129a6
Tinin AU <AUTinin@1cbit.ru>
2020-08-28 09:02:43 +0300

    New task "JadenCase"

---
+++ c#/ToJadeCase/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToJadeCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input;
+            Console.WriteLine("Please input your string:");
+            Input = Console.ReadLine();
+
+            string JadenCase = ToJadenCase(Input);
+            Console.WriteLine($"This is your string in Jaden case:\n {JadenCase}");
+            Console.ReadLine();
+
+        }
+
+        static string ToJadenCase(string phrase)
+        {
+            char whitespace = ' ';
+            string[] SubstringsArray = phrase.Split(whitespace);
+ 
+            string ReturnValue = "";
+            foreach(string Substring in SubstringsArray)
+            {
+                for(int i = 0; i < Substring.Length; i++)
+                {
+                    if(i==0) 
+                    {
+                        ReturnValue = ReturnValue + Substring[i].ToString().ToUpper();
+                    }  
+                    else
+                    {
+                        ReturnValue = ReturnValue + Substring[i];
+                    }
+                }
+                ReturnValue = ReturnValue + " ";
+               
+            }
+            return ReturnValue.TrimEnd(whitespace);
+        }
+    }
+}
