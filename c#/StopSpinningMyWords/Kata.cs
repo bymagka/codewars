5df174fcae4941b4171c16c3c44cfbd3a67b3a95
Tinin AU <AUTinin@1cbit.ru>
2020-08-29 20:00:02 +0300

    new task

---
+++ c#/StopSpinningMyWords/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopSpinningMyWords
+{
+    public class Kata
+    {
+        private int NumberOfKata;
+
+        public Kata(int numberOfKata)
+        {
+            this.NumberOfKata = numberOfKata;
+        }
+        public static string SpinWords(string sentence)
+        {
+            char Separator = ' ';
+            string[] StringArray = sentence.Split(Separator);
+            for(int i = 0; i < StringArray.Length;i++)
+            {
+                if (StringArray[i].Length >= 5)
+                {
+                    char[] CharArray = StringArray[i].ToCharArray();
+                    StringArray[i] = string.Join(String.Empty, CharArray.Reverse());
+                }
+            }
+ 
+            return string.Join(" ",StringArray);
+        }
+    }
+}
---
+++ c#/StopSpinningMyWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopSpinningMyWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your string");
+            string InputString = Console.ReadLine();
+            Console.WriteLine($"This is processed string:\n{Kata.SpinWords(InputString)}");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
