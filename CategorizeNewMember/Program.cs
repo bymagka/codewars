6dcf2f49d38bebea0a4ab36f0e44eecfe0c644c9
Tinin AU <AUTinin@1cbit.ru>
2020-09-17 16:34:56 +0300

    new done task

---
+++ CategorizeNewMember/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategorizeNewMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] List = new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } };
+            NewKata Members = new NewKata(List);
+            IEnumerable<string> Solution = Members.ReturnValue();
+
+            foreach(string s in Solution)
+            {
+                Console.WriteLine(s + " ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+    public interface Kata
+    {
+       string InputString {  set; }
+
+        IEnumerable<string> ReturnValue();
+
+
+    }
+
+    public class NewKata : Kata
+    {
+        public string InputString { get;  set; }
+
+        public int[][] data { get;  set; }
+
+        public NewKata(int[][] List,string input = "")
+        {
+            InputString = input;
+            data = List;
+        }
+
+        public IEnumerable<string> ReturnValue()
+        {
+            foreach (int[] Member in data)
+            {
+                if (Member[0] >= 55 && Member[1] > 7)
+                {
+                    yield return "Senior";
+
+                }
+                else
+                {
+                    yield return "Open";
+                }
+
+            }
+        }
+    }
+}
---
+++ c#/IsNumberPrime/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNumberPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int InputNumber;
+            Console.WriteLine("Please input your integer");
+            try
+            {
+                InputNumber = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                return;
+                
+            }
+            PrimeNumber newCheckableNumber = new PrimeNumber(InputNumber);
+            newCheckableNumber.Solution();
+            Console.WriteLine(newCheckableNumber.GetBoolOutput());
+            Console.ReadLine();
+            
+        }
+    }
+
+    public interface Kata
+    {
+        int IntegerInput {  set; }
+        bool BoolOutputValue { get; }
+        void Solution();
+
+        int GetIntegerInput();
+        bool GetBoolOutput();
+
+    }
+
+    public class PrimeNumber : Kata
+    {
+        public int IntegerInput { get; set; }
+
+        public bool BoolOutputValue { get; set; }
+
+        public bool GetBoolOutput()
+        {
+            return BoolOutputValue;
+        }
+
+        public int GetIntegerInput()
+        {
+            return IntegerInput;
+        }
+
+        public void Solution()
+        {
+
+            switch (IntegerInput)
+            {
+                case 0:
+                    BoolOutputValue = false;
+                    return;
+                case 1:
+                    BoolOutputValue = false;
+                    return;
+                case -1:
+                    BoolOutputValue = false;
+                    return;
+            }
+
+            if(IntegerInput < 0)
+            {
+                BoolOutputValue = false;
+                return;
+            }
+            double sqrtNumber = Math.Sqrt((double)IntegerInput);
+            BoolOutputValue = true;
+            for (double i = 2; i <= sqrtNumber; i++)
+            {
+                if((double)IntegerInput % i == 0)
+                {
+                    BoolOutputValue = false;
+                    return;
+                }
+            }
+        }
+
+        public PrimeNumber(int InputNumber)
+        {
+            IntegerInput = InputNumber;
+        }
+    }
+}
