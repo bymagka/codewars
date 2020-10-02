a09dfafdb76a4d32398cea5912ebb01371bd74d4
Tinin AU <AUTinin@1cbit.ru>
2020-10-02 16:22:29 +0300

    new task decipher ROT13

---
+++ c#/BlockSequence/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entr your long");
+            try
+            {
+                long Number = long.Parse(Console.ReadLine());
+                Console.WriteLine(BlockSeq.solve(Number).ToString());
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+            
+        }
+    }
+
+    public class BlockSeq
+    {
+
+        public static int solve(long n)
+        {
+            string Part = String.Empty;
+            string FinalString = String.Empty;
+
+            List<string> BlockList = new List<string>();
+            for (long x = 1;x<=n;x++)
+            {
+                Part = String.Empty;
+                for(long y = 1;y<=x;y++)
+                {
+                    Part = String.Concat(Part, y.ToString());
+                }
+                BlockList.Add(Part);
+            }
+
+            FinalString = String.Concat(BlockList);
+
+            long Numerator = 1;
+
+            foreach(char Symbol in FinalString)
+            {
+                if(Numerator == n) { return int.Parse(Symbol.ToString()); }
+                Numerator++;
+            }
+            return 0;
+        }
+    }
+}
---
+++ c#/ROT13/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROT13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your string");
+            string inputString = Console.ReadLine();
+
+            ROT13Implementation rot13 = new ROT13Implementation();
+            Console.WriteLine($"This is rot13 decipher: {rot13.DecipherROT13(inputString)}");
+            Console.ReadLine();
+        }
+    }
+
+    class ROT13Implementation
+    {
+        private List<char> Alphabet = new List<char>();
+        private List<char> ROT13 = new List<char>();
+
+        public ROT13Implementation()
+        {
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                Alphabet.Add(letter);
+            }
+        }
+
+        public string DecipherROT13(string InputString)
+        {
+            int Count = Alphabet.Count - 1;
+            foreach (char Symbol in InputString)
+            {
+
+                if (!Alphabet.Contains(char.ToUpper(Symbol)))
+                {
+                    ROT13.Add(Symbol);
+                    continue;
+                }
+
+                int indexOfSymbol = Alphabet.IndexOf(char.ToUpper(Symbol));
+
+                    if (Count - indexOfSymbol >= 13)
+                    {
+                        if (char.IsUpper(Symbol))
+                        {
+                            ROT13.Add(char.ToUpper(Alphabet[indexOfSymbol + 13]));
+                        }
+                        else
+                        {
+                            ROT13.Add(char.ToLower(Alphabet[indexOfSymbol + 13]));
+                        }
+                       
+                    }
+                    
+                else
+                {
+                    if (char.IsUpper(Symbol))
+                    {
+                        ROT13.Add(char.ToUpper(Alphabet[13 - (Count - indexOfSymbol + 1)]));
+                    }
+                    else
+                    {
+                        ROT13.Add(char.ToLower(Alphabet[13 - (Count - indexOfSymbol + 1)]));
+                    }
+                }
+                
+            }
+
+            return new string(ROT13.ToArray());
+        }
+
+
+    }
+}
